391cd85bf7d09789fa945b2aa9c314e9e8d5f2a2
Scott Stultz <scojoe85@gmail.com>
2026-04-27 10:59:12 -0600

    feat: scaffold Spring Boot backend

---
+++ backend/src/test/java/com/wewatch/api/WewatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wewatch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WewatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
